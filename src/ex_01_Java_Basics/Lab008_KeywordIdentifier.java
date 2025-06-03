75438a343dc3a3bcfecbb353c2760c0ec95280ac
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-03 23:47:03 +0530

    Lab008_KeywordIdentifire

---
+++ src/ex_01_Java_Basics/Lab008_KeywordIdentifier.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab008_KeywordIdentifier {
+    public static void main(String[] args) {
+        System.out.println("Keywords");
+        //6 keywords in this program
+    }
+}
