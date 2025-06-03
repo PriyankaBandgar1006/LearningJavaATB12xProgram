6dd2c8f9788de73674649580fea9afc8f18d8fa9
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-03 23:44:16 +0530

    Lab007_MethodImplementation

---
+++ src/ex_01_Java_Basics/Lab007_MethodImplementation.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+/*
+ * Author : Pramod
+ * Course : ATB12x
+ * This is a documentation comment
+ * */
+
+public class Lab007_MethodImplementation {
+    public static void main(String[] args) {
+
+
+            //method implementation
+            //print numbers
+            System.out.println("print numbers");
+
+    }
+}
