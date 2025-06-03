9268bb508b1bb383b16b6f547f8be065ce102c82
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-03 23:40:59 +0530

    Lab006_ShortCuts

---
+++ src/ex_01_Java_Basics/Lab006_ShortCuts.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab006_ShortCuts
+{
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        //Shortcut for main method is main
+        //Shortcut for system.out.println is sout
+        //Shortcut for duplicate is ctrl + D
+        System.out.println("Welcome");
+    }
+}
