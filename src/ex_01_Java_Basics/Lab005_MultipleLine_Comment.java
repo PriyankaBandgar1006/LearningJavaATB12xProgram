eadc442993b09f55e9a4b951ec8215a20d2a99c0
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-03 23:36:04 +0530

    Lab005_MultipleLine_Comments

---
+++ src/ex_01_Java_Basics/Lab005_MultipleLine_Comment.java
@@ -0,0 +1,30 @@
+package ex_01_Java_Basics;
+
+/*
+ * Author : Pramod Dutta
+ * Course : ATB12x
+ * Learning : Java
+ *
+ * */
+
+public class Lab005_MultipleLine_Comment {
+    public static void main(String[] args) {
+
+        /*
+         * Author : Pramod Dutta
+         * Course : ATB12x
+         * Learning : Java
+         *
+         * */
+        System.out.println("MultiLine comment");
+        /*
+         * Author : Pramod Dutta
+         * Course : ATB12x
+         * Learning : Java
+         *
+         * */
+    }
+
+
+    }
+
