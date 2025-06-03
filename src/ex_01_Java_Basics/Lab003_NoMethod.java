00e5de077c8b2a56ea9bb941911b4b6335088120
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-03 23:26:21 +0530

    Lab003_NoMethod

---
+++ src/ex_01_Java_Basics/Lab003_NoMethod.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab003_NoMethod {
+    public static void main(String[] args) {
+
+        //we can have a program with no main method.It will not run
+    }
+}
