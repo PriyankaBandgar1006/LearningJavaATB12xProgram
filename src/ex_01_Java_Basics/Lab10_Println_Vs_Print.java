416ab4438fd88b940cf57ad860946a69965dcfea
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-04 00:08:54 +0530

    Lab10_Println_Vs_Print

---
+++ src/ex_01_Java_Basics/Lab10_Println_Vs_Print.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab10_Println_Vs_Print {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.print("Bye");
+        System.out.println("Hello");
+        System.out.println("Promod");
+        System.out.print("Dutta");
+        System.out.println("Welcome");
+    }
+}
