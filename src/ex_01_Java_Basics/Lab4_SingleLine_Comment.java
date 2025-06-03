4fdeb06ea97a2f4bcf11bb3d42a1b199173db90c
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-03 23:28:53 +0530

    Lab004_SingleLine_Comment

---
+++ src/ex_01_Java_Basics/Lab4_SingleLine_Comment.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab4_SingleLine_Comment {
+    public static void main(String[] args) {
+
+        //This is single line comment, it will ignored by the compiler
+    }
+}
