14b407827157bf5ae8091fea37c577dd4ca8036d
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-04 09:58:56 +0530

    Lab14_Variables

---
+++ src/ex_02_Java_Basics/Lab14_Variables.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab14_Variables {
+    public static void main(String[] args) {
+        int age = 76;
+        age = 76+1 ;
+        age=78;
+        System.out.println(age);
+    }
+}
