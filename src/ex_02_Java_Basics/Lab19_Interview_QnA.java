1902504ca996343dc5a04d22dfcaa26392d88fa8
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-08 22:10:34 +0530

    Lab19_Interview_QnA

---
+++ src/ex_02_Java_Basics/Lab19_Interview_QnA.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab19_Interview_QnA {
+    public static void main(String[] args) {
+
+        int _2=20;
+        System.out.println(_2);
+        int _1 =20;
+        int _0=20;
+    }
+}
