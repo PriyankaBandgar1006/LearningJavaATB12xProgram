875f02d8c477dc119d5bcfa7d3ba93f0408ea05f
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-08 23:24:02 +0530

    Lab29_Literals

---
+++ src/ex_03_Litrerals/Lab29_Literals.java
@@ -0,0 +1,14 @@
+package ex_03_Litrerals;
+
+public class Lab29_Literals
+{
+    int age = 65;
+    // Literal - Integral
+
+    final float pi = 3.14f;
+    //  Literal - float
+
+
+    final int girl_age = 18;
+    //girl_age = 19;
+}
