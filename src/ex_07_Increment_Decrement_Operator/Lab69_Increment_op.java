29e3c2dfaa01db139b5d7903a06d757711234f3d
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-09 14:57:20 +0530

    Lab78_IncDec_Op_Task3

---
+++ src/ex_07_Increment_Decrement_Operator/Lab69_Increment_op.java
@@ -0,0 +1,9 @@
+package ex_07_Increment_Decrement_Operator;
+
+public class Lab69_Increment_op {
+    public static void main(String[] args) {
+        int a = 10;
+        int result = ++a;
+        System.out.println(result);
+    }
+}
---
+++ src/ex_07_Increment_Decrement_Operator/Lab76_IncDecOP_Task1.java
@@ -0,0 +1,11 @@
+package ex_07_Increment_Decrement_Operator;
+
+public class Lab76_IncDecOP_Task1 {
+    public static void main(String[] args) {
+        int i=11;
+        i = i++ + ++i; //11+13
+
+        System.out.println("i= "+i); // 24
+
+    }
+}
