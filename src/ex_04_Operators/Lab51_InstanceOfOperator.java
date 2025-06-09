4b4917da337a3c543f04bebcfac4ea4843f73b89
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-09 08:09:56 +0530

    Lab56_Short_Char

---
+++ src/ex_04_Operators/Lab51_InstanceOfOperator.java
@@ -0,0 +1,12 @@
+package ex_04_Operators;
+
+public class Lab51_InstanceOfOperator {
+    public static void main(String[] args) {
+        String s= new String("Priya");
+        int a= 10;
+        System.out.println(a);
+        System.out.println(s instanceof String);
+         // System.out.println(a instanceof String);
+    }
+    }
+
