078a489fda7bd0a029c5f4de34a342afc08c0945
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-11 23:37:08 +0530

    Lab122_While_Guessing_Game

---
+++ src/ex_11_WhileLoop/Lab116_While_Loop2.java
@@ -0,0 +1,11 @@
+package ex_11_WhileLoop;
+
+public class Lab116_While_Loop2 {
+    public static void main(String[] args) {
+        int modi = 1;
+        while (modi <= 15){
+            System.out.println("Modi will do 15 years");
+            modi ++;
+        }
+    }
+}
---
+++ src/ex_11_WhileLoop/Lab118_While.java
@@ -0,0 +1,11 @@
+package ex_11_WhileLoop;
+
+public class Lab118_While {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello, Priya");
+        }
+    }
+}
+//Infinite time it will run bcz condition is always true
+
