bb8017899f630df8bcae04b96cebdb0faa8414a1
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-11 23:58:17 +0530

    Lab125_Do_While_Real_Example

---
+++ src/ex_12_Do_While_Loop/Lab124_Diff_Between_While_And_DoWhile.java
@@ -0,0 +1,19 @@
+package ex_12_Do_While_Loop;
+
+public class Lab124_Diff_Between_While_And_DoWhile {
+    public static void main(String[] args) {
+        int a = 0;
+//        while ( a < 0){
+//            System.out.println(a);
+//            a++;
+//        }
+        // output is nothing bcz condition false
+
+        do {
+            System.out.println(a);
+            a++;
+        }while (a < 0);
+
+    }
+    }
+
---
+++ src/ex_12_Do_While_Loop/Lab125_Do_While_Real_Example.java
@@ -0,0 +1,15 @@
+package ex_12_Do_While_Loop;
+
+public class Lab125_Do_While_Real_Example {
+    public static void main(String[] args) {
+        // web Automation
+        int number = 0;
+        // 0 -> It can be 0 to 10 - elements or students
+        do {
+            System.out.println(number);
+            // callFunction(number)
+            // close the excel file, open the file
+            number++;
+        }while (number < 0);
+    }
+}
