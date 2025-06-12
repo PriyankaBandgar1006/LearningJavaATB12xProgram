d1e27cd896da1717857191b629ee098d5135e1f1
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-12 09:51:21 +0530

    Lab132_Arithmatic_Function

---
+++ src/ex_13_Function/Lab127_Method_Funtion.java
@@ -0,0 +1,14 @@
+package ex_13_Function;
+
+public class Lab127_Method_Funtion {
+    public static void main(String[] args) {
+        //step 2- call
+        name_of_the_func();
+    }
+    //Step 1 -definition
+
+    static void name_of_the_func(){
+        System.out.println("hi");
+    }
+    }
+
---
+++ src/ex_13_Function/Lab131_Interviewq.java
@@ -0,0 +1,25 @@
+package ex_13_Function;
+
+public class Lab131_Interviewq {
+    public static void main(String[] args) {
+
+    }
+    public static void main(String args) {
+
+    }
+
+    public static int main(int args) {
+        return 10;
+    }
+
+    public static float main(float args) {
+        return 10.3f;
+    }
+
+    public static boolean main(boolean args) {
+        return true;
+    }
+
+
+// we can write unlimited main function but only one is real rest of them are clones which does not match signature of jvm
+}
