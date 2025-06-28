52def6e89f505ab4d9a5764caf6b644b782eab31
PriyankaBandgar1006 <bandgarpriyanka412@gmail.com>
2025-06-28 12:08:21 +0530

    FirstProgram

---
+++ src/Test3/FirstProgram.java
@@ -0,0 +1,15 @@
+package Test3;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        String s="Hello World";
+
+        System.out.println(s.length());
+        System.out.println(s.charAt(0));
+        String sub = s.substring(6);
+        System.out.println(sub);
+
+
+
+    }
+}
